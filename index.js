4acc95169f832e6e2cf06496fabd7afc18def9bd
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-06-02 06:35:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setBestCustomer() {
 }
 
 function overwriteBestCustomer() {
-  var bestCustomer = "Bobby Flay";
+  var bestCustomer = "maybe bob";
 }
 
 function changeLeastFavoriteCustomer() {
